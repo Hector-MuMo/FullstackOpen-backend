1948c1f7286e4d4436a8c02d22d2beb9637952bf
Hector Muñoz M <hectormm@hobbio.co>
2024-08-19 11:29:15 -0600

    feat: Phonebook backend step 3

--- index.js
+++ index.js
@@ -37,6 +37,17 @@ app.get('/api/persons', (req, res) => {
     }
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = req.params.id;
+    const person = persons.find(item => item.id === id);
+
+    if (id && person) {
+        res.status(200).json(person);
+    } else {
+        res.status(404).json({ error: 'Person not found' });
+    }
+})
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
