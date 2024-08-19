47f1c11c703a6102015903f9c9e556542d89ddaa
Hector Muñoz M <hectormm@hobbio.co>
2024-08-19 11:14:15 -0600

    feat: create get all persons request for phonebook app | step 1

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require('express');
+
+const app = express();
+
+const persons = [
+    {
+        "id": "1",
+        "name": "Arto Hellas",
+        "number": "040-123456"
+    },
+    {
+        "id": "2",
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523"
+    },
+    {
+        "id": "3",
+        "name": "Dan Abramov",
+        "number": "12-43-234345"
+    },
+    {
+        "id": "4",
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122"
+    }
+]
+
+app.get('/api/persons', (req, res) => {
+    if (persons && persons.length > 0) {
+        return res.status(200).json(persons);
+    } else {
+        return res.status(404).json({ error: 'Not persons found' });
+    }
+})
+
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log('Server listening on port', PORT);
+})
