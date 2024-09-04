755356885777071ae43f94e5e4039ac4cf12246f
Hector Muñoz M <hectormm@hobbio.co>
2024-09-04 13:17:47 -0600

    feat: phonebook database steps 1 and 2

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
+const Person = require('./models/person');
 
 const app = express();
 
@@ -17,59 +19,64 @@ app.use(express.json());
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :log-post'));
 app.use(express.static('dist'));
 
-let persons = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     {
+//         "id": "1",
+//         "name": "Arto Hellas",
+//         "number": "040-123456"
+//     },
+//     {
+//         "id": "2",
+//         "name": "Ada Lovelace",
+//         "number": "39-44-5323523"
+//     },
+//     {
+//         "id": "3",
+//         "name": "Dan Abramov",
+//         "number": "12-43-234345"
+//     },
+//     {
+//         "id": "4",
+//         "name": "Mary Poppendieck",
+//         "number": "39-23-6423122"
+//     }
+// ]
 
 const RandomId = () => {
     return Math.floor(Math.random() * 10000).toString();
 }
 
 app.get('/api/info', (req, res) => {
-    res.status(200).send(`<p>Phonebook has info of ${persons.length} people<p><p>${Date()}</p>`).end();
+    Person.find({}).then(persons => {
+        res.status(200).send(`<p>Phonebook has info of ${persons.length} people<p><p>${Date()}</p>`).end();
+    })
 })
 
 app.get('/api/persons', (req, res) => {
-    if (persons && persons.length > 0) {
-        return res.status(200).json(persons);
-    } else {
-        return res.status(404).json({ error: 'Not persons found' });
-    }
+    Person.find({}).then(persons => {
+        if (persons && persons.length > 0) {
+            return res.status(200).json(persons);
+        } else {
+            return res.status(404).json({ error: 'Not persons found' });
+        }
+    })
+
 })
 
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id;
-    const person = persons.find(item => item.id === id);
+    Person.findById(id).then(person => {
+        if (id && person) {
+            res.status(200).json(person);
+        } else {
+            res.status(404).json({ error: 'Person not found' });
+        }
+    })
 
-    if (id && person) {
-        res.status(200).json(person);
-    } else {
-        res.status(404).json({ error: 'Person not found' });
-    }
 })
 
 app.post('/api/persons', (req, res) => {
     const person = req.body;
-    const isUnique = persons.find(item => item.name === person.name)
 
     if (!person?.name && !person?.number) {
         return res.status(400).json({
@@ -77,21 +84,24 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    if (isUnique) {
-        return res.status(400).json({
-            error: 'name must be unique'
+    Person.findOne({ "name": { $regex: `/${person?.name}/`, $options: 'i' } })
+        .then(person => {
+            if (!person) {
+                return res.status(400).json({
+                    error: 'Person already exists'
+                })
+            }
         })
-    }
 
-    const newPerson = {
-        id: RandomId(),
+    const newPerson = new Person({
         name: person.name,
         number: person.number
-    };
+    });
 
-    persons = [...persons, newPerson];
+    newPerson.save().then(savedPerson => {
+        return res.status(201).json(savedPerson);
+    })
 
-    return res.status(201).json(newPerson);
 })
 
 app.delete('/api/persons/:id', (req, res) => {
@@ -104,7 +114,7 @@ app.delete('/api/persons/:id', (req, res) => {
 })
 
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log('Server listening on port', PORT);
 })
